62aa31b7dab6942cdb23a6b2f0d94658a5133908
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2023-07-23 19:30:49 +0530

    file IO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,46 +85,55 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile f c = do
+  putStr "============ "
+  putStrLn f
+  putStrLn c
+  -- error "todo: Course.FileIO#printFile"
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles = void . sequence . ((<$>) (uncurry printFile))
+  -- error "todo: Course.FileIO#printFiles"
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile a = ((,) a) <$> readFile a
+  -- error "todo: Course.FileIO#getFile"
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles = sequence . ((<$>) getFile)
+  -- error "todo: Course.FileIO#getFiles"
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run path = do
+  (_, content) <- getFile path
+  let filePath = lines content
+  list <-  getFiles filePath
+  printFiles list
+  -- error "todo: Course.FileIO#run"
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = do
+  args <- getArgs
+  void $ sequence $ run <$> args
+  -- error "todo: Course.FileIO#main"
 
 ----
 
